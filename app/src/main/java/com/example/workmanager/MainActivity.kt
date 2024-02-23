99bec05208c76cb97dba426f6fb8378b6e6b8ac4
mert <mertyazi44@hotmail.com>
2024-02-23 22:06:35 +0300

    Expedited Work Commit

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.example.workmanager
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.work.BackoffPolicy
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
 import androidx.work.WorkManager
+import com.example.workmanager.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.Duration
+import android.Manifest
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        askNotificationPermission()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun makeWorkRequest() {
         val workRequest = OneTimeWorkRequestBuilder<CustomWorker>()
-            .setInitialDelay(Duration.ofSeconds(10))
+            .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
             .setBackoffCriteria(
                 BackoffPolicy.LINEAR,
                 Duration.ofSeconds(15)
@@ -26,4 +44,46 @@ class MainActivity : AppCompatActivity() {
             .build()
         WorkManager.getInstance(applicationContext).enqueue(workRequest)
     }
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                makeWorkRequest()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            Snackbar.make(
+                binding.clMain,
+                String.format(
+                    String.format(
+                        "Notifications permission not granted",
+                        getString(R.string.app_name)
+                    )
+                ),
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(getString(R.string.go_to_settings)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    val settingsIntent: Intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                    startActivity(settingsIntent)
+                }
+            }
+        }
+    }
+
 }
